42dcde4a15225e815dc4cc9cdbb7559f9f195cb4
Rikard Andersson <rikard.andersson@northvolt.com>
2024-08-01 08:57:17 +0200

    Switch to lazy

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- disable for now, not sure what it does
+vim.opt.autowrite = false
+
+-- language always english
+vim.cmd("language en_US")
